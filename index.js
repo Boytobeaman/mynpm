c645f0cf11a5f47cd408b8fd10cd02aca6f9afd7
luxingchao <luxingchao@luxingchaodeMacBook-Pro.local>
2021-05-31 16:30:34 +0800

    initial

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports={
+    eat:function(){
+        var arr=['巧克力火锅','海底捞','海盐泥巴奶茶','炭火烤肉',
+                 '国王栗子挞','年轮蛋糕','肥宅快乐水','草莓裸蛋糕','金沙酥','榴莲泡芙'];
+        var a=parseInt(Math.random()*10);
+        console.log(arr[a]);
+    }
+};
